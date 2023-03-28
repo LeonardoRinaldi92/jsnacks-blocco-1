36d483fb69036e1edd9bed0ec06481b29643e6f4
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-28 10:58:14 +0200

    struttura

---
+++ ex-1/main.js
@@ -0,0 +1,10 @@
+let numero1 = prompt("dicci un numero");
+let numero2 = prompt("dicci un numero");
+
+if (numero1 > numero2) {
+    document.writeln (numero1) 
+}else if (numero1 < numero2) {
+document.writeln (numero2)
+} else {
+    document.writeln ("che palle!")
+}
